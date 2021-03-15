eeef306ba995030786c7cfee205b7787cc99198c
David Morales <moralesl07@hotmail.com>
2021-03-15 01:27:01 -0500

    create adapter for socket-io in Redis

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './adapters/redis.adapter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import fetch from "node-fetch";
+declare var global: any;
+global.fetch = fetch;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4500);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
+  const port = parseInt(process.env.SERVER_PORT);
+  await app.listen(port);
 }
+
 bootstrap();
